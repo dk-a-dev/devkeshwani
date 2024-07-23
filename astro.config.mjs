d06c09b9a12527f9d87e31d7f4e28fb3bbb484a9
Dev Keshwani <dev.keshwani345@gmail.com>
2024-07-24 00:36:07 +0530

    feat: test on deployment :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  // output: 'server',
-  // adapter: vercel({
-  //   webAnalytics: { enabled: true }
-  // }),
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
